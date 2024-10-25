81616fcea359a11f1daafbd42fcb34424afbec21
dmitryzakharov <zakharovda95@gmail.com>
2024-10-25 03:29:11 +0300

    setting up database clients depending on environment

--- ControllerLayer/Program.cs
+++ ControllerLayer/Program.cs
@@ -11,7 +11,16 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Настройка логирования
+#region Настройка файла конфигурации
+
+builder.Configuration
+    .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+    .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json");
+
+#endregion
+
+#region Настройка логирования
+
 builder.Logging.ClearProviders();
 builder.Logging.AddConfiguration(builder.Configuration.GetSection("Logging"));
 builder.Logging.AddFile(options => builder.Configuration.GetSection("Logging.File").Bind(options));
@@ -22,32 +31,71 @@ if (builder.Environment.IsDevelopment())
     builder.Logging.AddDebug();
 }
 
-builder.Configuration
-    .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
-    .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json");
+#endregion
 
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseMySql(
-        builder.Configuration.GetConnectionString("DB_CS"), 
-        new MySqlServerVersion( new Version(9, 0, 0))));
+#region Настройка базы данных
+
+var databaseClient = builder.Configuration["DatabaseClient"];
+if (string.IsNullOrEmpty(databaseClient))
+    throw new NullReferenceException(nameof(databaseClient));
+
+var connectionString = databaseClient switch
+{
+    "musql" => builder.Configuration.GetConnectionString("DB_CS_MYSQL"),
+    "sql-light" => builder.Configuration.GetConnectionString("DB_CS_SQL_LIGHT"),
+    _ => string.Empty
+};
+if (string.IsNullOrEmpty(connectionString))
+    throw new NullReferenceException(nameof(connectionString));
+
+if (databaseClient == "mysql")
+{
+    builder.Services.AddDbContext<AppDbContext>(options =>
+        options.UseMySql(connectionString, new MySqlServerVersion(new Version(8, 0, 2))));
+}
+else if (databaseClient == "sql-light")
+{
+    builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlite(connectionString));
+}
+
+#endregion
+
+#region Профили маппера
 
 builder.Services.AddAutoMapper(
-    typeof(DictionaryAutoMapperProfile), 
+    typeof(DictionaryAutoMapperProfile),
     typeof(DictionaryElementAutoMapperProfile));
+
+#endregion
+
+#region Регистрация репозиториев
 
 // Репозитории
 builder.Services.AddScoped<IRepository<DictionaryModel, DictionaryModelBase>, DictionaryRepository>();
-builder.Services.AddScoped<IRepository<DictionaryElementModel, DictionaryElementModelBase>, DictionaryElementRepository>();
+builder.Services
+    .AddScoped<IRepository<DictionaryElementModel, DictionaryElementModelBase>, DictionaryElementRepository>();
 
-// UnitOfWorks
+#endregion
+
+#region Регистрация UnitOfWork
+
 builder.Services.AddScoped<IDictionaryUnitOfWork, DictionaryUnitOfWork>();
 
-// Сервисы
+#endregion
+
+#region Регистрация сервисов
+
 builder.Services.AddScoped<IDictionaryService, DictionaryService>();
 builder.Services.AddScoped<IDictionaryElementService, DictionaryElementService>();
 
+#endregion
+
+#region Прочее
+
 builder.Services.AddControllers();
 builder.Services.AddSwaggerGen();
+
+#endregion
 
 var app = builder.Build();
 
